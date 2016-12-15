cb89bdb3095ad416d57471ad4ff9e2c205eb85b1
jonathan.deng <dengjonathan@gmail.com>
2016-12-14 21:37:39 -0500

    feat (intro): add initial prompt sequence

---
+++ index.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt');
+
+const introHandler = require('./utils/introHandler');
+const inputHandler = require('./utils/inputHandler');
+process.stdin.setEncoding('utf8');
+
+console.log('hello world! I am minesweeper.');
+prompt.start();
+prompt.get([
+  {
+    name: 'rows',
+    message: 'How many rows?',
+    default: 10
+  },
+  {
+    name: 'cols',
+    message: 'How many cols?'
+    default: 10
+  },
+  {
+    name: 'mines',
+    message: 'How many mines?'
+    default: 3
+  }
+], introHandler);
